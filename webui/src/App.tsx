1e8d7ea8745fe55ef9c55de03c1c4d76bbda23d6
Anuchit Chalothorn <anoochit@gmail.com>
2026-05-09 05:08:30 +0700

    fix: remove non-existent AssistantModal export

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -1,5 +1,4 @@
 import { AssistantRuntimeProvider, useLocalRuntime, Thread } from "@assistant-ui/react";
-import { AssistantModal } from "@assistant-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
